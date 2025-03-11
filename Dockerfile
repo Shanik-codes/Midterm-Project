8e000ad35a213a48039ba857c88a935dc4aa3b63
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-03-11 18:47:03 -0400

    update

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 # Use an official OpenJDK runtime as a parent image
-FROM openjdk:11
+FROM openjdk:17
 
 # Set the working directory in the container
 WORKDIR /app
 
 # Copy the JAR file from the host to the container
-COPY target/myapp.jar myapp.jar
+COPY target/helloworld-1.0-SNAPSHOT.jar myapp.jar
 
 # Run the application
 ENTRYPOINT ["java", "-jar", "myapp.jar"]
--- src/main/java/com/example/helloworld/Main.java
+++ src/main/java/com/example/helloworld/Main.java
@@ -1,22 +1,40 @@
 package com.example.helloworld;
 
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public String sayHello() {
+
         return "Hello, World!";
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        logger.info("Program started");
+
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
+        logger.info("Name entered: " + name);
+
 
         System.out.println("Enter your major: ");
         String major = scanner.nextLine();
+        logger.info("Major entered: " + major);
 
-        System.out.print("Hello, " + name + " Congratulations on your " + major + " major!");
+        System.out.println("Hello, " + name + " Congratulations on your " + major + " major!");
+        logger.info("Greeting displayed");
+
 
         scanner.close();
+        logger.info("Scanner closed");
+        System.out.println("Program has ended.");
+        logger.info("Program ended");
+
+        System.exit(0);
     }
 }
